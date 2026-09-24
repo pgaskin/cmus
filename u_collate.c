f49301e14ab48b130356fdfd2a49edb2ffc71f12
Patrick Gaskin <patrick@pgaskin.net>
2026-09-24 00:14:20 -0400

    Remove unused u_strcoll functions
    Co-authored-by: Alexander Droste <alexander.droste@protonmail.com>

--- u_collate.c
+++ u_collate.c
@@ -28,58 +28,6 @@
 #include <string.h>
 #include <limits.h>
 
-int u_strcoll(const char *str1, const char *str2)
-{
-	int result;
-
-	if (using_utf8) {
-		result = strcoll(str1, str2);
-	} else {
-		char *str1_locale = NULL, *str2_locale = NULL;
-
-		convert(str1, -1, &str1_locale, -1, charset, "UTF-8");
-		convert(str2, -1, &str2_locale, -1, charset, "UTF-8");
-
-		if (str1_locale && str2_locale)
-			result = strcoll(str1_locale, str2_locale);
-		else
-			result = strcmp(str1, str2);
-
-		if (str2_locale)
-			free(str2_locale);
-		if (str1_locale)
-			free(str1_locale);
-	}
-
-	return result;
-}
-
-int u_strcasecoll(const char *str1, const char *str2)
-{
-	char *cf_a, *cf_b;
-	int res;
-
-	cf_a = u_casefold(str1);
-	cf_b = u_casefold(str2);
-
-	res = u_strcoll(cf_a, cf_b);
-
-	free(cf_b);
-	free(cf_a);
-
-	return res;
-}
-
-int u_strcasecoll0(const char *str1, const char *str2)
-{
-	if (!str1)
-		return str2 ? -1 : 0;
-	if (!str2)
-		return 1;
-
-	return u_strcasecoll(str1, str2);
-}
-
 char *u_strcoll_key(const char *str)
 {
 	char *result = NULL;
